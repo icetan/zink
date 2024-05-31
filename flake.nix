35580848c40f07bce4871488c11f6785a5078e61
Christopher Fredén <me@icetan.org>
2024-06-01 00:40:20 +0200

    Clean up flake

--- flake.nix
+++ flake.nix
@@ -1,46 +1,52 @@
 {
-  description = "A very basic flake";
+  description = "Zink it links your files!";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05-small";
+  outputs = { self, nixpkgs }@inputs:
+    let
+      mkOutput = system: builtins.mapAttrs (_: value: { ${system} = value; });
+      zigTargetMap = {
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-
-    # zig-json.url = "github:berdon/zig-json";
-    # zig-json.flake = false;
-
-    # zig-glob.url = "github:iCodeIN/glob-zig";
-    # zig-glob.flake = false;
-  };
-
-  outputs = { self, nixpkgs, ... }@inputs: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    zigDeps = pkgs.linkFarm "zink-deps" {
-      # inherit (inputs) zig-glob-fix; #zig-json;
-      # inherit zig-glob-fix;
-    };
-    shellHook = ''
-      ln -sfT ${zigDeps} libs
-    '';
-    app = pkgs.stdenv.mkDerivation {
-      name = "zink";
-      src = ./.;
-      buildInputs = builtins.attrValues {
-        inherit (pkgs) zig;
       };
-      buildPhase = ''
-        ${shellHook}
-        zig build -Drelease-small=true
-      '';
-      installPhase = ''
-        mv zig-out $out;
-      '';
-    };
-  in {
-    packages.x86_64-linux.default = app;
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = app.buildInputs ++ builtins.attrValues {
-        inherit (pkgs) zls;
-      };
-      inherit shellHook;
-    };
-  };
+    in
+    builtins.foldl'
+      (acc: system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          fs = pkgs.lib.fileset;
+          name = "zink";
+          version = "0.0.0";
+          pkg = pkgs.stdenv.mkDerivation {
+            name = "${name}-${version}";
+            src = fs.toSource {
+              root = ./.;
+              fileset = fs.unions [
+                ./src
+                ./vendor
+                ./build.zig
+                ./build.zig.zon
+              ];
+            };
+            buildInputs = builtins.attrValues {
+              inherit (pkgs) zig;
+            };
+            buildPhase = ''
+              zig build -Doptimize=ReleaseSmall -Dtarget=${system}
+            '';
+            installPhase = ''
+              mv zig-out $out;
+            '';
+          };
+        in
+        pkgs.lib.recursiveUpdate acc (mkOutput system {
+          apps.${name} = { type = "app"; program = "${pkg}/bin/zink"; };
+          apps.default = self.apps.${system}.${name};
+          packages.${name} = pkg;
+          packages.default = self.packages.${system}.${name};
+          devShells.default = pkgs.mkShell {
+            buildInputs = pkg.buildInputs ++ builtins.attrValues {
+              inherit (pkgs) zls;
+            };
+          };
+        }))
+      { } [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
 }
