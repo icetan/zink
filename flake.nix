a8a3bf7e8043278b457460e659c8829aac599251
Christopher Fredén <christopher.freden@zenseact.com>
2025-10-25 14:07:11 +0200

    Add github actions

--- flake.nix
+++ flake.nix
@@ -1,75 +1,83 @@
 {
-  description = "Zink it links your files!";
+  description = "Zink, it links your files!";
 
-  # nixpkgs
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  # inputs.nixpkgs-zig-0_13.url = "github:ExpidusOS/nixpkgs/feat/zig-0.13";
 
-  # devenv.sh
-  # inputs.devenv.url = "github:cachix/devenv";
-  # nixConfig.extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-  # nixConfig.extra-substituters = "https://devenv.cachix.org";
-
-  outputs = { self, nixpkgs, ... }:
-    (fn: builtins.foldl'
-      (acc: s: nixpkgs.legacyPackages.${s}.lib.recursiveUpdate
-        acc
-        (builtins.mapAttrs (_: value: { ${s} = value; }) (fn s)))
-      { })
-      (system:
+  outputs =
+    { self, nixpkgs, ... }:
+    (
+      fn:
+      builtins.foldl' (
+        acc: s:
+        nixpkgs.legacyPackages.${s}.lib.recursiveUpdate acc (
+          builtins.mapAttrs (_: value: { ${s} = value; }) (fn s)
+        )
+      ) { }
+    )
+      (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          # Broken :\ # pkgs-zig-0_13 = inputs.nixpkgs-zig-0_13.legacyPackages.${system};
-          fs = pkgs.lib.fileset;
 
-          inherit (pkgs) zig zls;
-
-          buildDeps = {
-            inherit zig;
+          buildDeps = { inherit (pkgs) zig; };
+          devDeps = buildDeps // {
+            inherit (pkgs) zls;
           };
 
-          name = "zink";
-          version = "0.0.1";
+          pname = "zink";
+          version = "0.1.0";
+          target = builtins.replaceStrings [ "darwin" ] [ "macos" ] system;
           pkg = pkgs.stdenv.mkDerivation {
-            name = "${name}-${version}";
-            src = fs.toSource {
-              root = ./.;
-              fileset = fs.unions [
-                ./src
-                ./vendor
-                ./build.zig
-                ./build.zig.zon
-              ];
-            };
+            name = "${pname}-${version}";
+            inherit pname version target;
+
+            src =
+              with pkgs.lib.fileset;
+              toSource {
+                root = ./.;
+                fileset = unions [
+                  ./src
+                  ./vendor
+                  ./build.zig
+                  ./build.zig.zon
+                ];
+              };
+
             buildInputs = builtins.attrValues buildDeps;
+
+            doCheck = true;
+            checkPhase = ''
+              zig build test -Dtest-filter=...
+            '';
+
             ZIG_GLOBAL_CACHE_DIR = "/tmp/zig-cache";
             buildPhase = ''
               mkdir -p $ZIG_GLOBAL_CACHE_DIR
-              zig build -Doptimize=ReleaseSmall -Dtarget=${system}
+              zig build -Doptimize=ReleaseSafe -Dtarget=${target}
             '';
+
             installPhase = ''
               mv zig-out $out;
             '';
           };
         in
         {
-          apps.${name} = { type = "app"; program = "${pkg}/bin/zink"; };
-          apps.default = self.apps.${system}.${name};
-          packages.${name} = pkg;
-          packages.default = self.packages.${system}.${name};
-
-          devShells.default = pkgs.mkShell {
-            buildInputs = builtins.attrValues (buildDeps // {
-              inherit zls;
-            });
+          apps.${pname} = {
+            type = "app";
+            program = "${pkg}/bin/zink";
           };
+          apps.default = self.apps.${system}.${pname};
 
-          # devenv.sh
-          # packages.devenv-up = self.devShells.${system}.default.config.procfileScript;
-          # devShells.default = inputs.devenv.lib.mkShell {
-          #   inherit inputs pkgs;
-          #   modules = [ (attrs: import ./devenv.nix (attrs // { deps = buildDeps; })) ];
-          # };
+          packages.${pname} = pkg;
+          packages.default = self.packages.${system}.${pname};
 
-        }) [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+          devShells.default = pkgs.mkShell { buildInputs = builtins.attrValues devDeps; };
+        }
+      )
+      [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
 }
