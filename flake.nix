a8daddb39691828494fb562f2481558f5090e3c2
Christopher Fredén <me@icetan.org>
2025-11-09 17:12:21 +0100

    Bump patch version

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
           };
 
           pname = "zink";
-          version = "0.1.0";
+          version = "0.1.1";
           target = builtins.replaceStrings [ "darwin" ] [ "macos" ] system;
           pkg = pkgs.stdenv.mkDerivation {
             name = "${pname}-${version}";
